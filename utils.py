530b4c7ab20038ba671fcfddbeae94109689ef0a
Rick Zhou <zhou0420@e.ntu.edu.sg>
2022-08-08 00:06:44 +0800

    add selector

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from project.solution.config import trading_config
+import joblib
+
 
 def print_resp_bid(resp_bid, is_print=False):
     if resp_bid is None:
@@ -21,3 +24,26 @@ def print_resp_bid(resp_bid, is_print=False):
         print(f'    latest_price = {p.latest_price}')  # 最新价格
         print(f'    share = {p.share}')  # 股数
         print(f'    tradable_share = {p.tradable_share}')  # 可交易数量
+
+
+def print_resp_user(resp_user, stage='before_bid', is_print=False):
+    if not is_print: return
+    print(f'\n[+] ---------------------------------------- user, {stage}')
+    print(f'    req_success = {resp_user.req_success}') # # 请求是否成功，True or False
+    print(f'    capital = {resp_user.capital}') # 当前总资产
+    print(f'    cash = {resp_user.cash}') # 总现金
+    print(f'    available_cash = {resp_user.available_cash}') # 可用现金
+    print(f'    pnl = {resp_user.pnl}') # 盈亏金额
+    print(f'    return_rate = {resp_user.return_rate}') # 收益率
+    print(f'    total_trade = {resp_user.total_trade}') # 当前天的总交易金额, 每天在ti == 33的时候会清0
+    print(f'    daily_capital = {resp_user.daily_capital}') # 过去每天的总资产余额
+    print(f'    daily_total_trade = {resp_user.daily_total_trade}') # 过去每天的总交易金额
+    print(f'    is_alive = {resp_user.is_alive}') # 是否爆仓， 如果爆仓，程序直接终止
+    print(f'    reason = {resp_user.reason}') # 爆仓原因
+
+
+def select_stock_info(stock_infos):
+    valid_stock_list = joblib.load(trading_config['valid_stock_path'])
+    selected_stock_infos = [stock_info for stock_info in stock_infos if stock_info.stock_id in valid_stock_list]
+    return selected_stock_infos
+
